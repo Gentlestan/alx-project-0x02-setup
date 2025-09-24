4230459efc7b34b14a36644d74c34ff3303c5e59
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-24 05:46:56 -0700

    added card.tsx

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,11 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({title, content}) => {
+    return(
+        <div className="border rounded-lg p-4 bg-white">
+            <h1 className="text-xl font-semibold">{title}</h1>
+        </div>
+    )
+}
+
+export default Card
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,11 @@
+import Card from "@/components/common/Card"
+
 const Home: React.FC = () => {
     return(
-        <div>
+        <div className="text-center">
             <h1>Welcome to Home Page</h1>
+            <Card title="Welcome" content="This is a welcome Card" />
+            <Card title="About" content="This is about Card" />
         </div>
     )
 }
