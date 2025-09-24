9da8e514b1bfe06907027cfad3cc43f4311edce6
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-23 22:37:32 -0700

    components interfaces

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            <h1>Header Elements here</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/Header"
+const Home: React.FC = () => {
+  return(
+    <div>
+      <Header/>
+      <main>Lets get this project working</main>
+    </div>
+  )
+}
+
+export default Home
