f5e1f456625960085c13dcbb5d580ce3a52d7527
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-24 21:05:00 -0700

    updated post.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
             <nav className="space-x-4">
                 <Link href="/home">Home</Link>
                 <Link href="/about">About</Link>
-                <Link href="/post">Post</Link>
+                <Link href="/posts">Posts</Link>
             </nav>
         </header>
     )
